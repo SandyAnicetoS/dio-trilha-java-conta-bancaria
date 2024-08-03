b9378116a91831de34049ba7a7aea8280e8fda3a
Sandy <sandysilva94@gmail.com>
2024-08-03 20:59:10 -0300

    desafio realizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o nome do cliente");
+        String nome = scanner.nextLine(); // permitindo duas palavras separadas por espaço
+
+        System.out.println("Por favor, digite o número da conta");
+        int conta = scanner.nextInt();
+
+        System.out.println("Por favor, digite a agência");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, informe seu saldo");
+        double saldo = scanner.nextDouble();
+
+        // explorando as formas de concatenação
+        System.out.println("Olá ".concat(nome).concat(", obrigado por criar uma conta em nosso banco, sua agência é ").concat(agencia).concat(", conta ")
+            + conta + " e seu saldo " + saldo + " já está disponivel pra saque.");
+    }   
 }
